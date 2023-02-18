20a81ce6d559f9e93668f63c129a78556b61d37f
NapoliN <sakashita.y.aa@m.titech.ac.jp>
2023-02-18 15:25:00 +0900

    add navbar

--- app/src/App.vue
+++ app/src/App.vue
@@ -25,6 +25,12 @@
 
     <v-main>
       <v-container>
+        <v-row class="mt-3">
+          <h1>一覧</h1>
+        </v-row>
+        <v-row class="mb-3">
+          <hr :color="$vuetify.theme.themes.light.colors.primary" width="100%">
+        </v-row>
         <v-row>
           <v-col v-for="shop in shops" cols="4">
             <ShopCard :shop="shop"></ShopCard>
