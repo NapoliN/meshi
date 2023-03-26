1ad8a38edf57fe457ba2c2505576c99e4a3311e6
NapoliN <sakashita.y.aa@m.titech.ac.jp>
2023-03-26 16:39:19 +0900

    modify icon

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <v-app id="inspire" class="custom-bg">
     <v-app-bar app color="primary">
-      <v-app-bar-nav-icon icon="mdi-menu" @click.stop="drawer=!drawer"></v-app-bar-nav-icon>
+      <v-app-bar-nav-icon icon="mdi-menu" @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
 
       <v-toolbar-title class="py-0"><v-btn size="ex-large" @click="toHome" class="py-0">飯ぶ</v-btn></v-toolbar-title>
 
@@ -18,49 +18,51 @@
               </v-row>
             </v-container>
           </v-card>
-      </v-menu>
+        </v-menu>
       </v-btn>
-      
+
     </v-app-bar>
 
-    <v-navigation-drawer
-        app
-        v-model="drawer"
-        color="secondary3"
-      >
+    <v-navigation-drawer app v-model="drawer" color="secondary3">
       <v-list density="compact" nav class="px-0">
-          <v-list-group>
-            <template v-slot:activator="{ props, isOpen }">
-              <v-list-item class="py-4" v-bind="props"> <v-icon :icon="isOpen ? 'mdi-menu-down' : 'mdi-menu-right'"></v-icon> 大岡山飯 </v-list-item>
-            </template>
-            <v-list-item class="py-4" @click="toTop" value="all">一覧</v-list-item>
-            <v-list-item class="py-4" @click="toArea" value="search_area"> 
-              <v-icon class="px-2" icon="mdi-map-marker-circle"></v-icon>
-              エリアから探す
-            </v-list-item>
-            <v-list-item class="py-4" @click="toGenre" value="search_genre">
-              <v-icon class="px-2" icon="mdi-basket"></v-icon>
-              ジャンルから探す
-            </v-list-item>
-            <v-list-item class="py-4" @click="toRandom">
-              <v-icon class="px-2" icon="mdi-dice-multiple"></v-icon>
-              今日のご飯(準備中)
-            </v-list-item>
-          </v-list-group>
+        <v-list-group>
+          <template v-slot:activator="{ props, isOpen }">
+            <v-list-item class="py-4" v-bind="props"> <v-icon
+                :icon="isOpen ? 'mdi-menu-down' : 'mdi-menu-right'"></v-icon> 大岡山飯 </v-list-item>
+          </template>
+          <v-list-item class="py-4" @click="toTop" value="all">
+            <v-icon class="px-2" icon="mdi-playlist-star"></v-icon>
+            一覧
+          </v-list-item>
+          <v-list-item class="py-4" @click="toArea" value="search_area">
+            <v-icon class="px-2" icon="mdi-map-marker-circle"></v-icon>
+            エリアから探す
+          </v-list-item>
+          <v-list-item class="py-4" @click="toGenre" value="search_genre">
+            <v-icon class="px-2" icon="mdi-basket"></v-icon>
+            ジャンルから探す
+          </v-list-item>
+          <v-list-item class="py-4" @click="toRandom">
+            <v-icon class="px-2" icon="mdi-dice-multiple"></v-icon>
+            今日のご飯(準備中)
+          </v-list-item>
+        </v-list-group>
 
-          <v-list-group>
-            <template v-slot:activator="{ props, isOpen }">
-              <v-list-item class="py-4" v-bind="props"> <v-icon :icon="isOpen ? 'mdi-menu-down' : 'mdi-menu-right'"></v-icon> 旅行飯(準備中) </v-list-item>
-            </template>
-            <v-list-item link class="pa-4" @click="toTripTop"> 一覧 </v-list-item>
-            <v-list-item class="pa-4"> 地域別 </v-list-item>
-            <v-list-item class="pa-4"> 時期別 </v-list-item>
-          </v-list-group>
-      </v-list>  
+        <v-list-group>
+          <template v-slot:activator="{ props, isOpen }">
+            <v-list-item class="py-4" v-bind="props"> <v-icon
+                :icon="isOpen ? 'mdi-menu-down' : 'mdi-menu-right'"></v-icon> 旅行飯(準備中) </v-list-item>
+          </template>
+          <v-list-item link class="pa-4" @click="toTripTop"> <v-icon class="px-2" icon="mdi-playlist-star"></v-icon>一覧
+          </v-list-item>
+          <v-list-item class="pa-4"> <v-icon class="px-2" icon="mdi-map-marker-circle"></v-icon> 地域別 </v-list-item>
+          <v-list-item class="pa-4"> <v-icon class="px-2" icon="mdi-calendar-month"></v-icon> 時期別 </v-list-item>
+        </v-list-group>
+      </v-list>
     </v-navigation-drawer>
     <v-main>
-      
-      
+
+
       <router-view></router-view>
       <v-container>
         <v-row justify="end">
@@ -77,25 +79,25 @@ export default {
   name: "App",
   data: () => ({
     drawer: false,
-    info_activator : true
+    info_activator: true
   }),
   methods: {
-    toHome(){
+    toHome() {
       this.$router.push("/")
     },
-    toTop(){
+    toTop() {
       this.$router.push("/ohokayama")
     },
-    toArea(){
+    toArea() {
       this.$router.push("/ohokayama/area")
     },
-    toGenre(){
+    toGenre() {
       this.$router.push("/ohokayama/genre")
     },
-    toRandom(){
+    toRandom() {
       this.$router.push("/ohokayama/random")
     },
-    toTripTop(){
+    toTripTop() {
       this.$router.push("/trip")
     }
   }
@@ -107,8 +109,8 @@ export default {
   background: rgb(var(--v-theme-base)) !important;
 }
 
-.v-application{
-    font-family: "M Plus 2" !important;
+.v-application {
+  font-family: "M Plus 2" !important;
 }
 </style>
 
